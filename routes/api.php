6d2fb5c9c62d92b379cba5874cda221bdea6313d
osamah <technoabg@gmail.com>
2023-03-05 15:59:00 +0300

    main

--- routes/api.php
+++ routes/api.php
@@ -125,6 +125,7 @@ Route::post(
 
 
 
+                $savedPathConvert =  base_path() . '/public/Attachments/' ."con". $name;
                 $savedPath =  base_path() . '/public/Attachments/' . $name;
 
 
@@ -134,9 +135,12 @@ Route::post(
                 // $command = "/var/www/html/ffmpeg/ffmpeg -i $ff -c:v h264_nvenc -pix_fmt yuv420p -vf scale=$resolution -b:v $bitRate -c:a copy $savedPath";
                 // $command = "ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -i $ff -c:a copy -c:v h264_nvenc -preset p6 -tune ll -b:v $bitRate -bufsize 5M -maxrate 10M -qmin 0 -g 250 -bf 3 -b_ref_mode middle -temporal-aq 1 -rc-lookahead 20 -i_qfactor 0.75 -b_qfactor 1.1 $savedPath";
 
-                $command = "ffmpeg -i $ff -c:v h264_nvenc -pix_fmt yuv420p -vf \"scale=$resolution,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p\" -b:v $bitRate -c:a copy $savedPath";
+                echo $commandConvert = exec("ffmpeg -hwaccel_device 0 -hwaccel cuda -i $ff -c:v h264_nvenc -preset slow $savedPathConvert");
+
+                $command = "ffmpeg -i $savedPathConvert -c:v h264_nvenc -pix_fmt yuv420p -vf \"scale=$resolution,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p\" -b:v $bitRate -c:a copy $savedPath";
                 echo exec($command);//
 
+                unlink($savedPathConvert);
                 // $video->save($format, base_path() . '/public/Attachments/' . $name);
                 return response()->json(['url' => url('/Attachments/' . $name)]);
             }
